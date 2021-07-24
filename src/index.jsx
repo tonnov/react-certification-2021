2373726ae55089935e557e1269f9a41444ece6d3
Antonio Nogueda <nogueda@gmail.com>
2021-07-24 14:25:58 -0500

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+// import App from './components/App';
 import './global.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <h1>Tono´s</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
