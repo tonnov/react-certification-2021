d137f07c8d37bc45b153fe7555b34b64f7b1ad52
tonnov <nogueda@gmail.com>
2021-11-19 00:28:09 -0600

    mocked login updated

--- src/api/Login.api.js
+++ src/api/Login.api.js
@@ -2,27 +2,30 @@ const mockedUsers = [
   {
     id: '123',
     name: 'Wizeline',
+    username: 'wizeline',
+    password: 'Rocks!',
     avatarUrl:
       'https://media.glassdoor.com/sqll/868055/wizeline-squarelogo-1473976610815.png',
   },
   {
     id: '6',
     name: 'Tono',
+    username: 'tono',
+    password: 'pass',
     avatarUrl:
       'https://avatars.githubusercontent.com/u/7376933?s=400&u=a9cfe9763d09edea21e3c02cb835e98384ec8195&v=4',
   },
 ];
 
-export default async function LoginApi(username, password) {
+export default async function LoginApi(userName, passWord) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (username === 'wizeline' && password === 'Rocks!') {
-        return resolve(mockedUsers.find((user) => user.id === '123'));
-      }
-      if (username === 'tono' && password === 'pass') {
-        return resolve(mockedUsers.find((user) => user.id === '6'));
-      }
-      return reject(new Error('Username or password invalid'));
+      const isUser = mockedUsers.find((user) => user.username === userName);
+
+      if (isUser && isUser.password === passWord) {
+        const { username, password, ...user } = isUser;
+        resolve(user);
+      } else reject(new Error('Username or password invalid'));
     }, 500);
   });
 }
